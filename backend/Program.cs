08b90fd5e933a123c65241cd0c27d65a84918368
Poobly <79822237+Poobly@users.noreply.github.com>
2025-04-27 16:45:25 -0400

    added login and signup route

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using backend.Data;
+using backend.Models;
+using backend.Services;
 
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
@@ -14,17 +16,19 @@ builder.Services.AddOpenApi();
 
 
 // connect to db
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<PasswordService>();
 
 var app = builder.Build();
 
 // Optional: Enable Swagger in development
 if (app.Environment.IsDevelopment())
 {
-	app.MapOpenApi(); 
+	app.MapOpenApi();
 }
 
 app.UseRouting();
@@ -39,31 +43,81 @@ app.UseStaticFiles(new StaticFileOptions
 
 app.MapWhen(context => context.Request.Path == "/", builder =>
 {
-    builder.Run(async context =>
-    {
-        var file = Path.Combine(Directory.GetCurrentDirectory(), "..", "frontend", "public", "index.html");
-        context.Response.ContentType = "text/html";
-        await context.Response.SendFileAsync(file);
-    });
+	builder.Run(async context =>
+	{
+		var file = Path.Combine(Directory.GetCurrentDirectory(), "..", "frontend", "public", "index.html");
+		context.Response.ContentType = "text/html";
+		await context.Response.SendFileAsync(file);
+	});
 });
 
-app.MapGet("/{username}", async (string username, AppDbContext db) => 
+// app.MapGet("/{username}", async (string username, AppDbContext db) => 
+// {
+// 	Console.WriteLine($"Requested username: {username}");
+// 	var user = await db.Users.Where(u => u.Name == username ).FirstOrDefaultAsync();
+
+// 	if (user is null)
+// 	{
+// 		return Results.NotFound();
+// 	}
+
+// 	return Results.Ok(user);
+// });
+
+app.MapGet("/api/user/{username}", async (string username, AppDbContext db) =>
 {
 	Console.WriteLine($"Requested username: {username}");
-	var user = await db.Users.Where(u => u.Name == username ).FirstOrDefaultAsync();
-	return Results.Ok(new { Message = $"Hello, {username}!" });
+	var user = await db.Users.Where(u => u.Name == username).FirstOrDefaultAsync();
+
+	if (user is null)
+	{
+		return Results.NotFound();
+	}
+
+	return Results.Ok(user);
 });
 
-// app.MapGet("/api/user")
+app.MapPost("/api/user/login", async (User user, UserService userService, PasswordService passwordService) => {
+	User? userT = null;
 
-// app.MapPost("create-user", async (st))
+	user.PasswordHash = passwordService.HashPassword(user.PasswordHash!);
+
+	try
+	{
+        userT = await userService.LoginAsync(user);
+	}
+	catch (ArgumentException e)
+	{
+		Results.NotFound(e.Message);
+	}
+	catch (UnauthorizedAccessException)
+	{
+		return Results.Unauthorized();
+	}
+
+    return Results.Ok(userT);
+});
+
+app.MapPost("/api/user/signup", async (User user, UserService userService) =>
+{
+	try
+	{
+		await userService.AddUserAsync(user);
+	}
+	catch (Exception e)
+	{
+		return Results.Conflict(e.Message);
+	}
+
+	return Results.Ok(user);
+});
 
 // Redirect to '/' for any non-matching routes
 app.MapFallback(async context =>
 {
-	
-    context.Response.Redirect("/", permanent: false);  // Redirect to "/"
-    await Task.CompletedTask;
+
+	context.Response.Redirect("/", permanent: false);  // Redirect to "/"
+	await Task.CompletedTask;
 });
 
 
