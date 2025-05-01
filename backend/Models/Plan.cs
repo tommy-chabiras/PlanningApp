d9dd0c4779c9b63bb7636d22f85bb2f3f9bdf684
Poobly <79822237+Poobly@users.noreply.github.com>
2025-05-01 18:31:00 -0400

    added code field to plans

--- backend/Models/Plan.cs
+++ backend/Models/Plan.cs
@@ -3,6 +3,7 @@ namespace backend.Models
 	public class Plan
 	{
 		public int Id { get; set; }
+		public required string Code { get; set; }
 		public required string Title { get; set; }
 		public string? Description { get; set; }
 		public required string Location { get; set; }
