5734dabfa6afc4148b4e93c4c3a949cae624de2a
Poobly <79822237+Poobly@users.noreply.github.com>
2025-05-01 18:30:02 -0400

    added login component

---
+++ frontend/src/components/Login.svelte
@@ -0,0 +1,11 @@
+<div>
+	<h2>Login</h2>
+	<form>
+		<input type="text" name="username" placeholder="Username" id="username" />
+		<input type="text" name="password" placeholder="Password" id="password" />
+	</form>
+</div>
+
+<style>
+	/* lab */
+</style>
