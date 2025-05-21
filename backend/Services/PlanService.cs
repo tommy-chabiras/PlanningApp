cbaebf4dda9a324a53e16f8fbd8b25b36048ce18
Poobly <79822237+Poobly@users.noreply.github.com>
2025-05-21 15:43:05 -0400

    added plan code generation method

--- backend/Services/PlanService.cs
+++ backend/Services/PlanService.cs
@@ -1,4 +1,5 @@
 using backend.Data;
+using backend.Dtos;
 using backend.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,37 @@ namespace backend.Services
 {
 	class PlanService(AppDbContext _db)
 	{
+		private static readonly Random _random = new();
+		private static readonly int _codeSize = 16;
+
+		private async Task<string> GeneratePlanCode()
+		{
+			char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+			char[] buffer = new char[_codeSize];
+			string code;
+
+			do
+			{
+				for (int i = 0; i < _codeSize; i++)
+				{
+					buffer[i] = chars[_random.Next(chars.Length)];
+				}
+				code = new string(buffer);
+			} while (await CheckCode(code));
+
+			return code;
+		}
+
+		public async Task<Boolean> CheckCode(string code)
+		{
+			return await _db.Plans.AnyAsync(p => p.Code == code);
+		}
+
+		public async Task<Plan?> GetPlanAsync(PlanRequest planR)
+		{
+			return await _db.Plans.Where(p => p.Code == planR.Code).FirstOrDefaultAsync();
+		}
+
 		public async Task<ICollection<User>> GetPlanUsersAsync(Plan plan)
 		{
 			return await _db.PlanUsers
@@ -14,8 +46,17 @@ namespace backend.Services
 				.ToListAsync();
 		}
 
-		public async Task<Plan> CreatePlanAsync(Plan plan)
+		public async Task<Plan> CreatePlanAsync(PlanRequest planR)
 		{
+			planR.Code = await GeneratePlanCode();
+			var plan = new Plan
+			{
+				Title = planR.Title,
+				Code = planR.Code,
+				Location = planR.Location,
+				Time = planR.Time
+			};
+			
 			await _db.Plans.AddAsync(plan);
 			await _db.SaveChangesAsync();
 			return plan;
