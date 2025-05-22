50b3efd804094d1e4fe2be2de02508c949bf80b5
Poobly <79822237+Poobly@users.noreply.github.com>
2025-05-22 10:29:54 -0400

    added dot for guest sign up requests and name to SignupRequest

--- backend/Dtos/SignupRequest.cs
+++ backend/Dtos/SignupRequest.cs
@@ -2,8 +2,15 @@ namespace backend.Dtos
 {
 	public class SignupRequest
 	{
+		public required string Name { get; set; }
 		public required string Username { get; set; }
 		public required string Password { get; set; }
 		public required string Email { get; set; }
+	}
+
+
+	public class SignupRequestGuest
+	{
+		public required string Name { get; set; }
 	}
 }
