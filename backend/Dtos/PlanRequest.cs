0d62a8ec50c9f1e68a5d39582bd60b9c5686283f
Poobly <79822237+Poobly@users.noreply.github.com>
2025-05-22 20:28:10 -0400

    moved attributes

--- backend/Dtos/PlanRequest.cs
+++ backend/Dtos/PlanRequest.cs
@@ -2,10 +2,10 @@ namespace backend.Dtos
 {
 	public class PlanRequest
 	{
-		public required string Title { get; set; }
 		public string? Code { get; set; }
-		public string? Description { get; set; }
+		public required string Title { get; set; }
 		public required string Location { get; set; }
 		public required DateTime Time { get; set; }
+		public string? Description { get; set; }
 	}
 }
